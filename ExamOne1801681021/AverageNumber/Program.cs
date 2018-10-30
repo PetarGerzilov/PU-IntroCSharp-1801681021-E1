9e63830eb1c8aeaee35b9c64d1b58d419fa8399f
Dexx <sexybat911@gmail.com>
2018-10-30 15:21:12 +0200

    Includes t1&t2 from ExamOne W5

---
+++ ExamOne1801681021/AverageNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AverageNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+
+        {
+            Console.WriteLine("Enter number thats between 0 and 255");
+            List<int> evenNumbers = new List<int>();
+
+            bool isZero = false;
+
+            while (!isZero) 
+            {
+                Console.WriteLine("Enter number");
+                int currentNumber = int.Parse(Console.ReadLine());
+                if (currentNumber == 0)  break; 
+                if (currentNumber % 2 == 0) evenNumbers.Add(currentNumber);
+            }
+            if(evenNumbers.Count>0)
+            {
+                Console.WriteLine($"Total even numbers:{evenNumbers.Count} => Average sum is: {evenNumbers.Average()} ");
+            }
+            else
+            {
+                Console.WriteLine("There are no even numbers");
+            }
+        }
+    }
+}
---
+++ ExamOne1801681021/RectangleArea/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The program will calculate an area of a rectangle!");
+            Random number = new Random();
+            int length = number.Next(1, 50);
+            int wigth = number.Next(51, 100);
+            int area = length * wigth;
+            Console.WriteLine($"The random area of the rectangle is: {area:0}");
+        }
+    }
+}
